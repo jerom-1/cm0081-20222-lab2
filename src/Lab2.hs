6cc3d7c114cfa0f40921413ac2c00c732f03d255
Alfonso Celimo Fierro López <81778388+Alfonso-Fierro@users.noreply.github.com>
2022-11-02 16:42:31 -0500

    Wall flags fixed, Type unary function, Prop check
    Fixed wall compile errors, every unary function takes a dummy arg, there is now one and only one property check

--- src/Lab2.hs
+++ src/Lab2.hs
@@ -2,31 +2,29 @@ module Main where
 import PRF
 import Numeric.Natural()
 import Test.QuickCheck(verboseCheck)
+
+
+--Instances of functions to test
 --
-unaryComp  = [predC,idenP]
+comparator :: [Nat->Nat->Nat]
+comparator = [predP,idenP,prodP,powP,addP]
 
-binaryCheck = [prodR,powR]
-unaryCheck  = [predR,idenR]
+check :: [Nat->Nat->Nat]
+check      = [predR,idenR,prodR,powR,addR]
 
-propCheckUn :: [Nat -> Nat] -> [Nat -> Nat] -> Nat -> Bool
---function, number, state
-propCheckUn [] _ _ = True
-propCheckUn (x:xs) (y:ys) i = (x i == y i) && propCheckUn xs ys i
 
+--Property check function
+--
 propCheckBi :: [Nat -> Nat -> Nat] -> [Nat -> Nat -> Nat] ->  Nat -> Nat -> Bool 
+propCheckBi [] [] _ _         = True
 propCheckBi [] _ _ _          = True
+propCheckBi _ [] _ _          = True
 propCheckBi (x:xs) (y:ys) i j = (x i j == y i j) && propCheckBi xs ys i j
 
 prop_dummy :: (Nat -> Nat -> Nat) -> Nat -> Nat -> Bool
 prop_dummy x i j = x i j == powP i j 
 
-
-prop_dummyuni :: (Nat -> Nat) -> Nat -> Bool
-prop_dummyuni x i = x i == facP i 
-
-
 main :: IO ()
-main = (verboseCheck $ propCheckUn unaryComp unaryCheck) 
+main = (verboseCheck $ propCheckBi comparator check) 
 --main = verboseCheck $ (propCheckBi binaryComp binaryCheck)
---main =print (propCheckBi binaryComp binaryCheck (fromNatural 12) (fromNatural 12))
 --main= verboseCheck $ prop_dummyuni facR
--- src/PRF.hs
+++ src/PRF.hs
@@ -21,6 +21,7 @@ recNat a _ Zero     = a
 recNat a h (Succ n) = h n (recNat a h n)
 
 --Addition Functions. 2 Implementations
+--
 addP :: Nat -> Nat -> Nat
 addP  Zero      n = n                      --Zero as module for addition 
 addP  (Succ m)  n = Succ (addP m n)        --Recursive performance 1+1+...	
@@ -30,13 +31,13 @@ addR m n = recNat n (\ _ y -> Succ y) m    --Nest m successors after n
 
 --Predecessor function. 2 implementations
 --
-predC :: Nat -> Nat
-predC Zero     = Zero
-predC (Succ n) = n
+predP :: Nat -> Nat -> Nat
+predP Zero _     = Zero
+predP (Succ n) _ = n
 
-predR :: Nat -> Nat
-predR Zero        = Zero
-predR (Succ n)    = recNat n (\ x _ -> x) Zero
+predR :: Nat -> Nat -> Nat
+predR Zero _     = Zero
+predR (Succ n) _ = recNat n (\ x _ -> x) Zero
 --Auxiliar function to create lists
 --return list of size n with m as every element
 --
@@ -56,9 +57,6 @@ prodR Zero _ = Zero                 --Base case for the recursion
 prodR (Succ m) n    = recNat n (\ _ y -> Succ y) (prodR m n)
 
 
-
-
-
 --Power functions. 2 implementations
 --
 powP :: Nat -> Nat -> Nat
@@ -69,27 +67,27 @@ powP x (Succ n)    = prodR x (powP x n)
 powR :: Nat -> Nat -> Nat
 powR _ Zero        = Succ Zero
 powR a (Succ Zero) = a
-powR x (Succ n)=recNat (powP x n) (\ _ y -> Succ y) (prodR (predC x) (powP x n))
+powR x (Succ n)=recNat (powP x n) (\ _ y -> Succ y) (prodR (predP x x) (powP x n))
 
 
 -- Factorial functions. 2 implementations
 --
-facP :: Nat -> Nat
-facP Zero        = (Succ Zero)
-facP (Succ x)    = prodP (Succ x) (facP x)
+facP :: Nat -> Nat -> Nat
+facP Zero _     = (Succ Zero)
+facP (Succ x) _ = prodP (Succ x) (facP x x)
 
-facR :: Nat -> Nat
-facR Zero = (Succ Zero)
-facR (Succ x)    = (recNat (facR x) (\ _ y -> Succ y) (prodR x (facR x)))
+facR :: Nat -> Nat -> Nat
+facR Zero _     = (Succ Zero)
+facR (Succ x) _ = (recNat (facR x x) (\ _ y -> Succ y) (prodR x (facR x x)))
 
 
 -- Identity function
 --
-idenP :: Nat -> Nat
-idenP x = x
+idenP :: Nat -> Nat -> Nat
+idenP x _ = x
 
-idenR :: Nat -> Nat
-idenR x = recNat x (\ _ w -> w) Zero
+idenR :: Nat -> Nat -> Nat
+idenR x _ = recNat x (\ _ w -> w) Zero
 
 
 -- Signum function (No recnat yet)
